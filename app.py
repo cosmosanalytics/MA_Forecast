30913d617d537c44b02c8be7f11f93d6fe91d3db
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-31 11:09:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 st.write('hello world')
-# All_Shipments_1_df = pd.read_csv('All_Shipments_1_simple.csv')
-# df = All_Shipments_1_df.copy() 
+All_Shipments_1_df = pd.read_csv('All_Shipments_1_simple.csv')
+df = All_Shipments_1_df.copy() 
 
 j_df = pd.read_csv("InView___Updated_List_of_5_Mil_Generics_for_Fcst_Testing__1_18_23_.csv")[['Generic','Plnt','4 Month Trend']]
 j_df['Plant-Generic'] = j_df['Plnt'].astype(str)+'-'+j_df['Generic'].astype(str)
