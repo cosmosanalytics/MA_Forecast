33c8d47d3c3c467074c543b437369cbbdf411a49
cosmosanalytics <100080151+cosmosanalytics@users.noreply.github.com>
2023-01-31 10:52:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import pandas as pd
+All_Shipments_1_df = pd.read_csv('All_Shipments_1_simple.csv')
+df = All_Shipments_1_df.copy() 
 
+j_df = pd.read_csv("InView___Updated_List_of_5_Mil_Generics_for_Fcst_Testing__1_18_23_.csv")[['Generic','Plnt','4 Month Trend']]
+j_df['Plant-Generic'] = j_df['Plnt'].astype(str)+'-'+j_df['Generic'].astype(str)
+j_df = j_df.drop(columns=['Plnt','Generic'])
+
+st.table(df)
+st.table(j_df)
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 with tab1:
